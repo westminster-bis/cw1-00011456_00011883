dd511a04d1ef68c38ce1505debfdc68d2c2f1c01
MansurIsakov <mansurisakov.dev@gmail.com>
2021-11-22 14:12:48 +0500

    feat: questions & outputs

---
+++ checkMarkResult.py
@@ -0,0 +1,19 @@
+# On time? Yes - Output
+# On time? No - Within 24 hours? Yes - Late submission? Is there a valid reason? No - Output
+# On time? No - Within 24 hours? Yes - Late submission? Is there a valid reason? Yes - Accepted MC? Output
+# On time? No - Within 24 hours? No - Submitted within 5 days? No - Is there a valid reason with MC non submission? No | Yes - Output
+# On time? No - Within 24 hours? No - Submitted within 5 days? Yes - Is there a valid reason with MC submission? No - Output
+# On time? No - Within 24 hours? No - Submitted within 5 days? Yes - Is there a valid reason with MC submission? Accepted? No | Yes - Output
+
+outputs = ["Full Mark", "Mark = 0", "Minus 10 marks from overall mark but not below 40"]
+questions = [
+    "Course work submitted on time? ",
+    "Within 24 hours? ",
+    "Is there a valid reason? ",
+    "Accepted MC Claim for within 24 hour? ",
+    "Submitted within 5 days? ",
+    "Is there a valid reason? ",
+    "Is there a valid reason? ",
+    "Accepted MC Claim? ",
+    "Accepted MC Claim? ",
+]
